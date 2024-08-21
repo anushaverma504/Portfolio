3a20017f6551a236472f255917c2afbc1255617d
Anusha Verma <anusha.verma@mittalsoftwarelabs.com>
2024-08-21 23:19:57 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,131 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Hello, I'm Anusha Verma." ,
+    // "role": "Web Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am a Full-Stack Developer and RPA Consultant skilled in React, Automation Anywhere, and PowerBI. I create dynamic web applications and automate business processes, combining expertise in JavaScript, HTML, CSS, SQL, and VB Script. My focus is on delivering efficient, scalable, and high-quality solutions.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/anusha-verma/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/anushaverma504",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am Anusha Verma, a dedicated Full-Stack Developer and RPA Consultant with a Master’s in Computer Applications from Galgotias University and a Bachelor’s in Sciences from the University of Delhi. My expertise lies in developing dynamic web applications and automating complex business processes to enhance operational efficiency. Currently, as an RPA Developer, I specialize in designing and executing automation scripts that significantly reduce manual efforts and improve project efficiency. My technical skills encompass a range of programming languages including C++, Python, and JavaScript, along with frameworks such as React and Automation Anywhere.",
+    // "address":"anushaverma504@gail.com",
+    // "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Galgotias University",
+        "specialization":"Master of Computer Applications",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2023",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"University of Delhi",
+        "specialization":"Bachelor of Sciences",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2021",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Mittal Software Labs Ltd",
+        "specialization":"Software Developer",
+        "MonthOfJoining":"August",
+        "YearOfJoining":"2023",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":"",
+        "currentlyWorkimg":true
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    // "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javscript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"NodeJs"
+      },
+      {
+        "skillname":"MY SQL"
+      },
+      {
+        "skillname":"C++"
+      },
+      {
+        "skillname":"Automation Anywhere"
+      },
+      {
+        "skillname":"VB Script"
+      },
+      {
+        "skillname":"Powerbi"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // },
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // }
+    ],
+    "contact":"anushaverma504@gmail.com"
+  }
+  
+  export default resumeData
